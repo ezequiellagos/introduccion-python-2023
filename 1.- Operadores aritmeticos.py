fb36a4c7c9d214b713409ebd908fd73a2ef02745
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2023-04-26 15:42:00 -0400

    Clase 26-04-2023 Periodo 1

---
+++ 1.- Operadores aritmeticos.py
@@ -0,0 +1,25 @@
+print("Suma")
+print(2 + 3)
+
+print("Resta")
+print(5 - 2)
+
+print("Multiplicacion")
+print(5 * 2)
+
+print("Division")
+print(15 / 3)
+
+print("Modulo")
+print(7 % 3)
+
+print("Exponente")
+print(2 ** 3)
+
+print("Orden de predencia para operaciones aritmeticas")
+print("Parentesis, potencias, raices, multiplicación, división, suma y la resta")
+print(5 + 5 * 2)
+
+print("Tarea")
+print("Como hacer una raiz cuadrada")
+print("Averiguar que es la división entera")
---
+++ 2.- Variables.py
@@ -0,0 +1,17 @@
+numero = 3
+print(numero)
+
+numero1 = 5
+numero2 = 3
+
+print(numero1 + numero2)
+
+resultado = numero1 + numero2
+print(resultado)
+
+# Reglas del nombre de las variables
+# 1.- No pueden empezar por un numero
+# 2.- Recomendacion, escribir siempre en minúscula
+# 3.- Separar las palabras mediante un guión bajo _
+# 4.- No se puede escribir espacio
+# 5.- No pueden cotener caracteres especiales
